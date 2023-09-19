21c396186dd615236c4c4aca88c9c894059d6ba9
Asher <ashersam116@gmail.com>
2023-09-19 06:22:23 -0900

    added cors for the api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 import multer from "multer";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -13,6 +14,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// Define CORS options
+const corsOptions = {
+  origin: "https://localhost:3000", // Replace with the actual frontend domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable credentials (cookies, headers) to be included in the request
+  optionsSuccessStatus: 204, // Set the status code for successful preflight requests
+};
+
+// Use the cors middleware with the defined options
+app.use(cors(corsOptions));
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../client/public/upload");
