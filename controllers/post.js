6118f698ad0764cf53af3683ecd8319b66d88731
Asher <ashersam116@gmail.com>
2023-09-23 04:34:40 -0900

    delete post

--- controllers/post.js
+++ controllers/post.js
@@ -53,42 +53,56 @@ export const addPost = (req, res) => {
   });
 };
 
-export const deletePost = (req, res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
-
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const postId = req.params.id;
-    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
-
-    db.query(q, [postId, userInfo.id], (err, data) => {
-      if (err) return res.status(403).json("You can delete only your post!");
-
-      return res.json("Post has been deleted!");
-    });
-  });
-};
-
-// export const updatePost = (req, res) => {
+// export const deletePost = (req, res) => {
 //   // const token = req.cookies.access_token;
 //   // if (!token) return res.status(401).json("Not authenticated!");
 
-//   // jwt.verify(token, "jwtkey", (err, userInfo) => { });
-//   // if (err) return res.status(403).json("Token is not valid!");
+//   // jwt.verify(token, "jwtkey", (err, userInfo) => {
+//   //   if (err) return res.status(403).json("Token is not valid!");
+//   // });
 
 //   const postId = req.params.id;
-//   const q =
-//     "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+//   const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
 
-//   const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+//   db.query(q, [postId, userInfo.id], (err, data) => {
+//     if (err) return res.status(403).json("You can delete only your post!");
 
-//   db.query(q, [...values, postId, userInfo.id], (err, data) => {
-//     if (err) return res.status(500).json(err);
-//     return res.json("Post has been updated.");
+//     return res.json("Post has been deleted!");
 //   });
 // };
+
+export const deletePost = (req, res) => {
+  // const token = req.cookies.access_token;
+  // if (!token) return res.status(401).json("Not authenticated!");
+
+  // jwt.verify(token, "jwtkey", (err, userInfo) => {
+  //   if (err) return res.status(403).json("Token is not valid!");
+  // });
+
+  const postId = req.params.id;
+  const userId = req.body.userId; // Extract the userId from the request body
+
+  const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+  db.query(q, [postId, userId], (err, data) => {
+    if (err) {
+      // Check if the error indicates that the post doesn't exist or the user doesn't have permission
+      if (err.code === "ER_ROW_NOT_FOUND") {
+        return res
+          .status(404)
+          .json("Post not found or you don't have permission to delete it.");
+      }
+      return res.status(500).json(err);
+    }
+
+    // Check if no rows were affected, indicating the user doesn't have permission
+    if (data.affectedRows === 0) {
+      return res.status(403).json("You can delete only your post!");
+    }
+
+    return res.json("Post has been deleted!");
+  });
+};
 
 export const updatePost = (req, res) => {
   // const token = req.cookies.access_token;
