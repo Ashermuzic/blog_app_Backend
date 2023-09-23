00a9974de6deff6ce50dfd1b68b5d5c503a5f7b7
Asher <ashersam116@gmail.com>
2023-09-23 04:24:31 -0900

    update post fixing

--- controllers/post.js
+++ controllers/post.js
@@ -71,20 +71,49 @@ export const deletePost = (req, res) => {
   });
 };
 
+// export const updatePost = (req, res) => {
+//   // const token = req.cookies.access_token;
+//   // if (!token) return res.status(401).json("Not authenticated!");
+
+//   // jwt.verify(token, "jwtkey", (err, userInfo) => { });
+//   // if (err) return res.status(403).json("Token is not valid!");
+
+//   const postId = req.params.id;
+//   const q =
+//     "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+
+//   const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+
+//   db.query(q, [...values, postId, userInfo.id], (err, data) => {
+//     if (err) return res.status(500).json(err);
+//     return res.json("Post has been updated.");
+//   });
+// };
+
 export const updatePost = (req, res) => {
   // const token = req.cookies.access_token;
   // if (!token) return res.status(401).json("Not authenticated!");
 
-  // jwt.verify(token, "jwtkey", (err, userInfo) => { });
-  // if (err) return res.status(403).json("Token is not valid!");
+  // jwt.verify(token, "jwtkey", (err, userInfo) => {
+  //   if (err) return res.status(403).json("Token is not valid!");
+  // });
 
   const postId = req.params.id;
+  const userId = req.body.userId; // Extract the userId from the request body
+
   const q =
-    "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+    "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=? WHERE `id` = ? AND `uid` = ?";
 
-  const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.img,
+    req.body.cat,
+    postId,
+    userId, // Use the extracted userId
+  ];
 
-  db.query(q, [...values, postId, userInfo.id], (err, data) => {
+  db.query(q, values, (err, data) => {
     if (err) return res.status(500).json(err);
     return res.json("Post has been updated.");
   });
